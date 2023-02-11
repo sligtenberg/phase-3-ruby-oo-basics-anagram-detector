f4fcbe741eedf7bd4a6d0c9b1cb15365d976b392
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-11 00:01:40 -0500

    commit tree 793927fafb500e67fa30bfbb9608473aa33f898e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    def match word_list
+        word_list.filter do |test_word|
+            test_word.chars.sort == @word.chars.sort
+            # binding.pry
+        end
+    end
+
+    def initialize(word)
+        @word = word
+    end
+end
